b001f06717e76b90dc7d6bb8a05fda4cd8f397f1
Thanh <nguyenthanhtoan8544@gmail.com>
2024-10-08 21:03:13 +0700

    Add project files.

---
+++ DoAnhViet_KTFE/Controllers/Product_Details_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnhViet_KTFE.Controllers
+{
+    public class Product_Details_Controller : Controller
+    {
+        // GET: Product_Details_
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DoAnhViet_KTFE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAnhViet_KTFE.MvcApplication" Language="C#" %>
